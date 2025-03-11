8425202f09ac4653f8324d95350be65a10bce4c3
Carmen Gomez Rodriguez <cagomez-@c1r14s6.42madrid.com>
2025-03-11 20:23:20 +0100

    main

--- src/main.c
+++ src/main.c
@@ -1,56 +1,63 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cagomez- <cagomez-@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/10 19:26:01 by cagomez-          #+#    #+#             */
-/*   Updated: 2025/03/11 19:25:20 by cagomez-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "push_swap.h"
+
 
 int	init_stack(int argc, char **argv, t_stack **stack)
 {
-	int		i;
-	int		j;
-	int		n;
+	int	i;
+	int	n;
 	t_stack	*new;
-	char	**num;
 
-	num = NULL;
-	//ft_printf("%d\n", argc);
-	i = 1;
-	while (i < argc)
+	if (ft_check_duplicates(argc, argv))
 	{
-		j = 0;
-		num = ft_split(argv[i], ' ');
-		while(num[j])
+		i = 1;
+		while (i < argc) 
 		{
-			n = ft_atoi_push(num[j]);
+			n = ft_atoi_push(argv[i]);
 			ft_printf("%d\n", n);
 			new = ft_stacknew(n);
 			ft_stackadd_back(stack, new);
-			j++;
+			//ft_stackclear(stack);
+			i++;
 		}
-		//ft_stackclear(stack);
+		//ft_printf("Número de nodos: %d\n", ft_stacksize(stack) - 1);
+	}
+	return (1);
+	/*while (i < argc)
+	{
+		n = atoi(argv[i]);
+		new = ft_stacknew(n);
+		ft_stackadd_back(stack, new); //hacemos que n sea el nuevo nodo creado 
+		//podemos no declarar t_stack *new y directamente poner ft_stackadd_back(stack, ft_stacknew(n)) una función como parámetro 
+									//void ft_stackadd_back(t_stack **alst, t_stack *new)
 		i++;
 	}
-		//ft_printf("Número de nodos: %d\n", ft_stacksize(stack) - 1);
-	return (1);
-}
+	return (1);*/
+}	
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	t_stack	*stack_a;
-
-	stack_a = malloc(sizeof(t_stack)); // Reserva memoria para el primer nodo
+    t_stack *stack_a = malloc(sizeof(t_stack)); // Reserva memoria para el primer nodo
 	if (!stack_a)
-		return (0);
+		return (1);
 	init_stack(argc, argv, &stack_a);// en init_stack mis argumentos los convierto a enodos de la estructura del stack_a
+   
 	ft_stacksize(stack_a);
-	ft_printf("Número de nodos: %d\n", ft_stacksize(stack_a) - 1);
-	return (0);
+    printf("Número de nodos: %d\n", ft_stacksize(stack_a) - 1);
+
+    //Liberar memoria
+	//ft_stackclear(stack_a);
+    // current = stack_a; // Reiniciar el puntero astacka para liberar memoria
+    // t_stack *temp;
+    // while (current)	
+	// {
+    //     temp = current; // Almacenar el nodo actual
+    //     current = current->next; // Mover al siguiente nodo
+    //     free(temp); // Liberar el nodo actual
+    // }
+
+    return (0);
 }
+
